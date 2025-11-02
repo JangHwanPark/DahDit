42224910747ab8cfe549ebbe284bfed1ca7a0ed8
JangHwan <wkd5878@gmail.com>
2025-11-02 20:42:15 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by wkd58 on 25. 11. 2..
+//
+
+#include <iostream>  // 입출력 기능을 사용하기 위한 헤더
+
+int main() {         // 프로그램의 시작점 (main 함수)
+    std::cout << "Hello, World!" << std::endl;  // 콘솔에 문자열 출력
+    return 0;        // 프로그램 정상 종료
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by wkd58 on 25. 11. 2..
+//
+
+#ifndef DAHDIT_MAIN_H
+#define DAHDIT_MAIN_H
+
+
+class main
+{
+};
+
+
+#endif //DAHDIT_MAIN_H
